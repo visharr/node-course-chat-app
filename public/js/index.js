c972c540fcb1a178b196ae1811199daf490d6509
Vashar <vishal.pilani@live.com>
2019-03-28 16:51:50 +0530

    add form for messages and showing in browser

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,28 @@ socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 
-socket.on('newMessage', function (email) {
-    console.log('newMessage', email);
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+
+    jQuery('#messages').append(li);
 })
+
+// socket.emit('createMessage', {
+//     from: 'vishal',
+//     text: 'hi'
+// }, function (data) {
+//     console.log('got it', data);
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'vishal',
+        text: jQuery('[name=message]').val()
+    }, function (data) {
+
+    });
+
+});
